c20b48016773ce0502ebe9dfb70a51763a812002
arjunkumaramtrar-website <arjunkumaramtrar@gmail.com>
2026-03-18 22:29:06 +0530

    practic chal rhi hai

---
+++ jquery.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    // console.log($);
+    // $('h1').dblclick(function () {
+    //     console.log("button click hua");
+    //     $(this).hide();
+    // })
+    // $('h2').mouseenter(function () {
+    //     console.log("mouse enter hua");
+    //     $(this).hide();
+    // })
+    // $('.wiki').hide(2000, function () {
+
+    // })
+    // $('.wiki').show(2000, function () {
+
+    // })
+    $('.but').click(function () {
+        $('.wiki').toggle(1000);
+    })
+})
